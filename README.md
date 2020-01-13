081ee6dad90f4947f0dd2416efca0f23ae8464c5
Gabe Marquez <gabemarquez@protonmail.com>
2020-01-13 13:15:27 -0500

    Main scaffolding

---
+++ README.md
@@ -0,0 +1,13 @@
+# TODO
+
+## Design Work
+- [x] favicon design
+- [ ] Figma mobile view
+
+## Front End
+- [x] replace favicon
+- [ ] Background color
+- [ ] Display TODO lists
+
+## Back End
+- [ ]
---
+++ src/components/App/component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.scss'
+
+export const App = () =>
+  <div className='App'>
+    <header className='App-header'>
+        <h1>Gabe Marquez</h1>
+    </header>
+  </div>
