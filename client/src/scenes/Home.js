6ce93a50ac63e4b9f65ece394dbf5af03ed072e2
Gabe Marquez <gabemarquez@protonmail.com>
2020-01-28 05:42:24 -0500

    Home.js : add main-container class

--- client/src/scenes/Home.js
+++ client/src/scenes/Home.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Debug from 'debug'
-
 import {
   withTheme,
   withAnimation
 } from 'hocs'
 
 const debug = Debug('scenes:Home')
-
 const Code = props => <span {...props}>code</span>
-
-const DumbHeight = withAnimation(Code)
 
 const HomeNav = () => (
   <Nav defaultActiveKey='/home'>
-    <Nav.Item>
-      <Nav.Link eventKey='code'>
-        <DumbHeight
-          aid='code'
-          startValue={12}
-          endValue={40}
-          property='height'
-          animatedStyle={{
-            height: 12
-          }}
-        >
-          code
-        </DumbHeight>
-      </Nav.Link>
-    </Nav.Item>
-
     <Nav.Item>
       <Nav.Link href='/untitled-daw' eventKey='untitled-daw'>
         Untitled Daw
@@ -46,7 +26,7 @@ const HomeNav = () => (
 )
 
 export const Home = withTheme(({ theme }) =>
-  <div className='main-container'>
+  <div className='home container main-container'>
     <header>
       <h1>Gabe Marquez</h1>
     </header>
