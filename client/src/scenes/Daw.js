daa1912b77293239592b6c1cfbf0b652be84b12d
Gabe Marquez <gabemarquez@protonmail.com>
2020-01-28 05:41:33 -0500

    Daw.js : wrap in withTheme, add main-container class` git status ` ;

--- client/src/scenes/Daw.js
+++ client/src/scenes/Daw.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Debug from 'debug'
 import { Descriptor } from '../components'
-
 import {
-  withDevMode
+  withDevMode,
+  withTheme
 } from 'hocs'
 
 const debug = Debug('scenes:daw')
 
 const DawInterface = props => {
-  return <div>
+  return <div className='daw container main-container'>
     <Descriptor>Daw Interface</Descriptor>
   </div>
 }
 
-export const UntitledDaw = withDevMode(DawInterface)
+export const UntitledDaw = withTheme(withDevMode(DawInterface))
 
 debug('loaded')
